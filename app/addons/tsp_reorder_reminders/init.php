e33f19949275da57041caeadbf4cd59fc50e3ae0
Sharron Denice <sharron@me.com>
2013-11-18 15:54:35 -0600

    * For version 4.0.3 - privilege_descriptions table was deleted and adjustments had to be made

--- app/addons/tsp_reorder_reminders/init.php
+++ app/addons/tsp_reorder_reminders/init.php
@@ -4,7 +4,7 @@
  *
  * @package		TSP Re-Order Reminders for CS-Cart
  * @filename	init.php
- * @version		1.0.0
+ * @version		1.0.1
  * @author		Sharron Denice, The Software People, LLC on 2013/02/09
  * @copyright	Copyright © 2013 The Software People, LLC (www.thesoftwarepeople.com). All rights reserved
  * @license		Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported (http://creativecommons.org/licenses/by-nc-nd/3.0/)
