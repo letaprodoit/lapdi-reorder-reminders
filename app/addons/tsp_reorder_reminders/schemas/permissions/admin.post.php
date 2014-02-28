87de3cb7adfbe14d22e40ba5ce950ff35a204c87
Sharron Denice <sharron@thesoftwarepeople.com>
2014-02-28 12:57:06 -0600

    - Fixing issue with permissions

--- app/addons/tsp_reorder_reminders/schemas/permissions/admin.post.php
+++ app/addons/tsp_reorder_reminders/schemas/permissions/admin.post.php
@@ -4,7 +4,7 @@
  *
  * @package		TSP Re-Order Reminders for CS-Cart
  * @filename	admin.post.php
- * @version		1.0.1
+ * @version		1.1.2
  * @author		Sharron Denice, The Software People, LLC on 2013/02/09
  * @copyright	Copyright © 2013 The Software People, LLC (www.thesoftwarepeople.com). All rights reserved
  * @license		Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported (http://creativecommons.org/licenses/by-nc-nd/3.0/)
@@ -14,10 +14,8 @@
 $schema = array();
 
 $schema['reminders'] = array (
-	'permissions' => 'manage_reminders',
+    'permissions' => array ('POST' => 'manage_reminders')
 );
-$schema['tools']['modes']['update_status']['param_permissions']['table_names']['addon_tsp_reorder_reminders'] = 'manage_reminders';
-
 
 return $schema;
 
