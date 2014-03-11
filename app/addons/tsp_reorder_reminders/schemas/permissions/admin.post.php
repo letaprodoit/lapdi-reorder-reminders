c0404d9146a8be30fa398d3c2db68faabcf3f26f
Sharron Denice <sharron@thesoftwarepeople.com>
2014-03-11 16:12:15 -0500

    - Only update the scheme if the scheme value is not empty

--- app/addons/tsp_reorder_reminders/schemas/permissions/admin.post.php
+++ app/addons/tsp_reorder_reminders/schemas/permissions/admin.post.php
@@ -4,7 +4,7 @@
  *
  * @package		TSP Re-Order Reminders for CS-Cart
  * @filename	admin.post.php
- * @version		1.1.4
+ * @version		1.1.5
  * @author		Sharron Denice, The Software People, LLC on 2013/02/09
  * @copyright	Copyright © 2013 The Software People, LLC (www.thesoftwarepeople.com). All rights reserved
  * @license		Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported (http://creativecommons.org/licenses/by-nc-nd/3.0/)
@@ -12,12 +12,15 @@
  * 
  */
 
-$schema['reminders'] = array (
-	'permissions' => 'manage_reminders',
-);
-
-$schema['tools']['modes']['update_status']['param_permissions']['table']['addon_tsp_reorder_reminders'] = 'manage_reminders';
-
-return $schema;
+if (!empty($schema))
+{
+	$schema['reminders'] = array (
+			'permissions' => 'manage_reminders',
+	);
+	
+	$schema['tools']['modes']['update_status']['param_permissions']['table']['addon_tsp_reorder_reminders'] = 'manage_reminders';
+	
+	return $schema;
+}
 
 ?>
