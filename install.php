2847d0ddde0f20e50b0ad1a675b1692ba656614b
Sharron Denice <sharron@thesoftwarepeople.com>
2014-03-15 17:49:22 -0500

    - Updated install script to install to a theme directory other than basic - Updated notifications to properly log cron events

--- install.php
+++ install.php
@@ -4,29 +4,44 @@
  *
  * @package		TSP Re-Order Reminders for CS-Cart
  * @filename	install.php
- * @version		1.1.0
+ * @version		1.1.6
  * @author		Sharron Denice, The Software People, LLC on 2013/02/09
  * @copyright	Copyright © 2013 The Software People, LLC (www.thesoftwarepeople.com). All rights reserved
  * @license		Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported (http://creativecommons.org/licenses/by-nc-nd/3.0/)
  * @brief		Script to copy addon files to their respective locations
  * 
  */
-$target_loc 		= '';
+
 $source_loc			= '.';
+$target_loc 		= '';
+$theme_folder_name  = '';
 
 $addon_name 		= 'tsp_reorder_reminders';
 $addon_dir			= 'app/addons';
 $theme_backend_dir	= 'design/backend';
-$theme_frontend_dir	= 'design/themes/basic';
-$theme_var_dir		= 'var/themes_repository/basic';
+
+
+$source_theme_frontend_dir	= 'design/themes/basic';
+$source_theme_var_dir		= 'var/themes_repository/basic';
+
+$target_theme_frontend_dir	= 'design/themes/'.$theme_folder_name;
+$target_theme_var_dir		= 'var/themes_repository/'.$theme_folder_name;
 
 if (empty( $target_loc ))
 {
-	echo "You must specify a target location on line #14 of this script.";
+	echo "You must specify a target location on line #16 of this script.";
 }//end if
 elseif (!file_exists( $target_loc ))
 {
-	echo "The $target_loc specified does not exist on line #14 of this script.";
+	echo "The $target_loc (target location) specified does not exist on line #16 of this script.";
+}//end elseif
+elseif (empty( $theme_folder_name ))
+{
+	echo "You must specify a theme folder name location (ie `basic`) on line #17 of this script.";
+}//end elseif
+elseif(!file_exists( "$target_loc/$target_theme_frontend_dir" ))
+{
+	echo "The $target_theme_frontend_dir (target theme) specified does not exist on line #17 of this script.";
 }//end elseif
 else
 {
